37332f33b06b61df81873a6cc1bfd7bd4899fa16
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-04-01 10:32:38 +0200

    Updated DbContext - Created DbSets and Constructor

--- SystemSolution/WebApi/Data/IdentityDbContext.cs
+++ SystemSolution/WebApi/Data/IdentityDbContext.cs
@@ -1,6 +1,13 @@
-﻿namespace WebApi.Data
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi.Data
 {
-    public class IdentityDbContext
+    public class IdentityDbContext : DbContext
     {
+        public DbSet<Game> Games { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public IdentityDbContext(DbContextOptions<IdentityDbContext> options) : base(options) { }
     }
 }
