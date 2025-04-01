22ce8134a28d6886a9462b09361bc03bc10c823d
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-04-01 10:43:52 +0200

    Created and Updated GameDTO - Created Basic Properties

---
+++ SystemSolution/WebApi/DTOs/GameDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi.DTOs
+{
+    public class GameDTO
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? DevTeam { get; set; }
+    }
+}
