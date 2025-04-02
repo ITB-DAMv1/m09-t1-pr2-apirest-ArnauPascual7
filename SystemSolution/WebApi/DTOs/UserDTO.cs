0d3902f82e44e351f188905741bbfa9821c1a2bc
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-04-02 12:50:20 +0200

    refactor(api): Updated User Model

--- SystemSolution/WebApi/DTOs/UserDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApi.DTOs
-{
-    public class UserDTO
-    {
-        public int Id { get; set; }
-        public string? Email { get; set; }
-        public string? Password { get; set; }
-    }
-}
--- SystemSolution/WebApi/Data/IdentityDbContext.cs
+++ SystemSolution/WebApi/Data/IdentityDbContext.cs
@@ -6,7 +6,7 @@ namespace WebApi.Data
     public class IdentityDbContext : DbContext
     {
         public DbSet<Game> Games { get; set; }
-        public DbSet<User> Users { get; set; }
+        public DbSet<AppUser> Users { get; set; }
 
         public IdentityDbContext(DbContextOptions<IdentityDbContext> options) : base(options) { }
     }
