8ad5eed90de8e7daac871100b40f355dd4ad0a6d
Arnau Pascual <arnau.pascual.7e8@itb.cat>
2025-04-03 11:48:00 +0200

    refactor(api): changed dbcontext inheritance to identitydbcontext<appuser>

--- SystemSolution/WebApi/Data/AppDbContext.cs
+++ SystemSolution/WebApi/Data/AppDbContext.cs
@@ -1,12 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using WebApi.Models;
 
 namespace WebApi.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<AppUser>
     {
         public DbSet<Game> Games { get; set; }
-        public DbSet<AppUser> Users { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     }
