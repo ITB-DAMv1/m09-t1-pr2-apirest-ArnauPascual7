67670766df93f00aa7a2f200a78f3707fcbba14e
Arnau Pascual Carretero <apascualcarretero@gmail.com>
2025-04-21 13:53:55 +0200

    feature(client): uservotess

---
+++ SystemSolution/WebRazorPage/Models/VoteDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace WebRazorPage.Models
+{
+    public class VoteDTO
+    {
+        public int GameId { get; set; }
+        public string UserId { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
--- SystemSolution/WebRazorPage/Pages/UserVotes.cshtml.cs
+++ SystemSolution/WebRazorPage/Pages/UserVotes.cshtml.cs
@@ -1,8 +1,10 @@
+using System.Diagnostics;
 using System.Net.Http;
 using System.Security.Claims;
 using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebRazorPage.Models;
 
 namespace WebRazorPage.Pages
 {
@@ -12,7 +14,7 @@ namespace WebRazorPage.Pages
         private readonly ILogger<UserVotesModel> _logger;
 
         [BindProperty]
-        public List<string> Votes { get; set; } = new List<string>();
+        public Dictionary<string, DateTime> GamesVoted { get; set; } = new Dictionary<string, DateTime>();
         public string? Token { get; set; }
         public string? ErrorMessage { get; set; }
 
@@ -29,15 +31,57 @@ namespace WebRazorPage.Pages
             var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
             var client = _httpClientFactory.CreateClient("ApiGameJam");
-            var response = await client.GetAsync("api/games");
-            if (response.IsSuccessStatusCode)
+
+            var token = HttpContext.Session.GetString("AuthToken");
+            if (!string.IsNullOrEmpty(token))
             {
-                var json = await response.Content.ReadAsStringAsync();
-                Games = JsonSerializer.Deserialize<List<Game>>(json, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+                client.DefaultRequestHeaders.Authorization =
+                    new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+            }
+
+            var votesResponse = await client.GetAsync("api/games/votes");
+            if (votesResponse.IsSuccessStatusCode)
+            {
+                var votesJson = await votesResponse.Content.ReadAsStringAsync();
+                var votes = JsonSerializer.Deserialize<List<VoteDTO>>(votesJson, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+                if (votes != null)
+                {
+                    foreach (var vote in votes)
+                    {
+                        if (vote.UserId == userId)
+                        {
+                            var gameResponse = await client.GetAsync($"api/games/{vote.GameId}");
+                            if (gameResponse.IsSuccessStatusCode)
+                            {
+                                var gameJson = await gameResponse.Content.ReadAsStringAsync();
+                                var game = JsonSerializer.Deserialize<Game>(gameJson, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+                                if (game != null)
+                                {
+                                    GamesVoted.Add(game.Title, vote.Date);
+                                }
+                                else
+                                {
+                                    ErrorMessage = "No s'ha trobat el joc.";
+                                    _logger.LogError(ErrorMessage);
+                                }
+                            }
+                            else
+                            {
+                                ErrorMessage = $"Error en la càrrega del joc: {votesResponse.StatusCode}";
+                                _logger.LogError(ErrorMessage);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    ErrorMessage = "No s'han trobat vots.";
+                    _logger.LogError(ErrorMessage);
+                }
             }
             else
             {
-                ErrorMessage = $"Error en la càrrega dels jocs: {response.StatusCode}";
+                ErrorMessage = $"Error en la càrrega dels vots: {votesResponse.StatusCode}";
                 _logger.LogError(ErrorMessage);
             }
         }
